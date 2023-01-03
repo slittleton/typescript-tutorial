61d745f722be909d443842be205ccc584bb3d26a
slittleton <littletonltp@gmail.com>
2023-01-03 13:40:44 -0700

    data files project

--- ts6-dataFiles/build/index.js
+++ ts6-dataFiles/build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("------- This is the main file -----------------");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+let matchesWon = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        matchesWon++;
+    }
+    if (match[2] === 'Man United' && match[5] === 'A') {
+        matchesWon++;
+    }
+}
+console.log(` Man United won ${matchesWon} games`);
--- ts6-dataFiles/src/index.ts
+++ ts6-dataFiles/src/index.ts
@@ -1,1 +1,24 @@
-console.log("------- This is the main file -----------------")
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+
+let matchesWon: number = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    matchesWon++;
+  }
+  if (match[2] === 'Man United' && match[5] === 'A') {
+    matchesWon++;
+  }
+}
+
+
+console.log(` Man United won ${matchesWon} games`);
